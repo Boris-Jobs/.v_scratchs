30e3ca876218938025e8cac500febdb90c96869d
boris sigma <1322553126@qq.com>
2024-12-11 23:57:57 +0800

    Update fastapi_basic_server.py

--- llm/6_FastAPI/fastapi_basic_server.py
+++ llm/6_FastAPI/fastapi_basic_server.py
@@ -1,11 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
 
-model_name = "Qwen/Qwen-7B-Chat"
+model_name = "Qwen/Qwen-1_8B-Chat"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    device_map="auto",
+    device_map="cuda",
     trust_remote_code=True
 ).eval()
 response, history = model.chat(tokenizer, "你听说过哪些大模型安全对齐的经典方法？", history=None)
